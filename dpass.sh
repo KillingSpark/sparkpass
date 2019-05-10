33c7098b6c018253ffeb67006f599b3b7b6c74d0
Moritz Borcherding <moritz.borcherding@web.de>
2019-05-11 01:25:06 +0200

    switched from dmenu to rofi

--- dpass.sh
+++ dpass.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
-RES="$(target/debug/sparkpass ls $1 | dmenu)"
+RES="$(target/debug/sparkpass ls $1 | rofi -dmenu)"
 
 NAME=$(echo $RES | cut -d " " -f 2)
 
+# "recursion" into deeper sub directories
 [[ "${RES}" =~ ^DIR ]] && ./dpass.sh "$1"/"${NAME}"
 
+# end result into clipboard
 [[ "${RES}" =~ ^ENT ]] && target/debug/sparkpass show "$1"/$NAME | xclip -sel clip
