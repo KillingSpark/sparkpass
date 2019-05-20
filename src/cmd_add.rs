f91f6811325adb3accd36d079a0334f3dc263cc3
Maximilian von Gaisberg <github@vongaisberg.de>
2019-05-20 16:17:38 +0200

    Add interactive entering during insert

--- src/cmd_add.rs
+++ src/cmd_add.rs
@@ -1,23 +1,56 @@
-use crate::util::{Options, prepare_entry_path, add_entry};
+extern crate rpassword;
+
 use crate::transform;
+use crate::util::{add_entry, prepare_entry_path, Options};
 
 use std::path;
 
+fn read_password_from_terminal(name: String) -> String {
+    print!("Enter content for {}: ", name);
+    let pass = rpassword::read_password().unwrap();
+    return pass.to_owned();
+}
+
 pub fn cmd_add(opts: &Options, prefix: &path::Path, enc_params: &transform::EncryptionParams) {
-    if opts.args.len() != 2 {
-        println!("Incorrect number of arguments. Want: 'path_new, content'  Got: {}", opts.args.len());
+    if opts.args.len() < 1 || opts.args.len() > 2 {
+        println!(
+            "Incorrect number of arguments. Want: 'path_new, [content]'  Got: {}",
+            opts.args.len()
+        );
         return;
     }
 
     let relative_path = prepare_entry_path(opts.args[0].as_str());
-    if opts.verbose {println!("Adding Entry: {}, Content: {}", relative_path, opts.args[1]);}
+    let pwd = if opts.args.len() == 1 {
+        if !opts.interactive {
+            println!("No key given and interactive mode deactivated");
+            return;
+        }
 
+        if opts.verbose {
+            println!("Only one argument was given, requesting the password via interactive input");
+        }
 
-    match add_entry(prefix, path::Path::new(relative_path), opts.args[1].as_str(), opts.force, enc_params){
-        Ok(_) => {},
+        read_password_from_terminal(opts.args[0].to_string())
+    } else {
+        opts.args[1].to_string()
+    };
+
+    if opts.verbose {
+        println!("Adding Entry: {}", relative_path);
+    }
+
+    match add_entry(
+        prefix,
+        path::Path::new(relative_path),
+        pwd.as_str(),
+        opts.force,
+        enc_params,
+    ) {
+        Ok(_) => {}
         Err(e) => {
             println!("An error occurred while adding the entry: {}", e);
             return;
-        },
+        }
     }
 }
